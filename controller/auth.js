fa3c8c746590053dd54b650fc0c6c0a336d039d0
pawan <pawankumar7310@gmail.com>
2023-08-10 19:27:19 +0530

    vercel

--- controller/auth.js
+++ controller/auth.js
@@ -2,17 +2,18 @@ var jwt = require("jsonwebtoken");
 const model = require("../model/user");
 const User = model.user;
 const bcrypt = require("bcrypt");
+require("dotenv").config();
 
 //create products
 exports.signUp = async (req, resp) => {
   try {
     const user = new User(req.body);
-    var token = jwt.sign({ email: req.body.email }, "shhhhh");
+    var token = jwt.sign({ email: req.body.email }, process.env.JWTSECRET);
     const hash = bcrypt.hashSync(req.body.password, 10);
     user.token = token;
     user.password = hash;
     const doc = await user.save();
-    console.log({ doc });
+
     resp.status(201).json(doc);
   } catch (err) {
     console.log(err);
@@ -25,7 +26,7 @@ exports.login = async (req, resp) => {
     const doc = await User.findOne({ email: req.body.email });
     const isAuth = bcrypt.compareSync(req.body.password, doc.password);
     if (isAuth) {
-      var token = jwt.sign({ email: req.body.email }, "shhhhh");
+      var token = jwt.sign({ email: req.body.email }, process.env.JWTSECRET);
       doc.token = token;
       const user = await doc.save();
       console.log({ user });
